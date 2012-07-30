1988e3d6a05d35f17f16ca791710f9920597c0ad
Michael Sims <michael.h.sims@gmail.com>
2012-07-30 12:59:24 -0500

    Add EditText to action bar as a collapsible action view

--- src/main/java/com/example/ActionBarSpike/HelloAndroidActivity.java
+++ src/main/java/com/example/ActionBarSpike/HelloAndroidActivity.java
@@ -1,15 +1,24 @@
 package com.example.ActionBarSpike;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import com.android.debug.hv.ViewServer;
 
 public class HelloAndroidActivity extends Activity {
 
     private static final String TAG = "ActionBarSpike";
+
+    private Handler handler = new Handler();
+
+    private EditText searchEditText;
 
     /**
      * Called when the activity is first created.
@@ -32,7 +41,43 @@ public class HelloAndroidActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.menu, menu);
+
+        final MenuItem search = menu.findItem(R.id.menu_search);
+        searchEditText = (EditText) search.getActionView();
+
+        search.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                enterSearchMode();
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                exitSearchMode();
+                return true;
+            }
+        });
+
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void enterSearchMode() {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG, "EXPAND!");
+                searchEditText.requestFocus();
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(searchEditText, InputMethodManager.SHOW_IMPLICIT);
+            }
+        });
+    }
+
+    private void exitSearchMode() {
+        searchEditText.setText("");
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(searchEditText.getWindowToken(), 0);
     }
 
     @Override
